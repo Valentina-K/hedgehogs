1c0ba58e14cfb0e044ac0ca48e74229f42b32126
Valentina-K <kvalentyna@hotmail.com>
2024-11-28 10:44:48 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace hedgehogs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hedg = {100,10,7};
+            int result = GetMinNumberOfMeeting(0, hedg);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static int GetMinNumberOfMeeting(int color, int[] hedgehogs)
+        {
+            int zeroValue = 0;
+            foreach(int hedgehog in hedgehogs)
+            {
+                if (hedgehog == 0) zeroValue++;
+            }
+            if (zeroValue > 1) return -1;
+            int[] choice = new int[2];
+            int j = 0;
+            for (int i = 0; i < hedgehogs.Length; i++)
+            {
+                if (i == color) continue;
+                else
+                {
+                    choice[j] = hedgehogs[i];
+                    j++;
+                }
+            }
+
+            return Math.Max(choice[0], choice[1]);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("hedgehogs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hedgehogs")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b073b378-5dd5-43d2-a158-8c4dfbbf2b40")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
